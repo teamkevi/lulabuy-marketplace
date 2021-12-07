8d7637598b0ba0e7fa577481373bb93cdbc2e542
Gabriel Low <lowgabriel@ymail.com>
2021-12-07 23:24:35 +0800

    added input

--- public/js/app.js
+++ public/js/app.js
@@ -19832,7 +19832,51 @@ var render = function () {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", [_vm._v("Index")])
+  return _c("div", [
+    _c("div", { staticClass: "flex relative w-full" }, [
+      _c("input", {
+        staticClass:
+          "border-1 border-gray-300 bg-red transition h-12 pl-3 pr-16 rounded focus:outline-none w-full text-black text-lg ",
+        attrs: { type: "search", name: "search", placeholder: "Search" },
+      }),
+      _vm._v(" "),
+      _c(
+        "button",
+        {
+          staticClass: "absolute right-2 top-3 mr-4",
+          attrs: { type: "submit" },
+        },
+        [
+          _c(
+            "svg",
+            {
+              staticClass: "text-black h-6 w-6 fill-current",
+              staticStyle: { "enable-background": "new 0 0 56.966 56.966" },
+              attrs: {
+                xmlns: "http://www.w3.org/2000/svg",
+                "xmlns:xlink": "http://www.w3.org/1999/xlink",
+                version: "1.1",
+                id: "Capa_1",
+                x: "0px",
+                y: "0px",
+                viewBox: "0 0 56.966 56.966",
+                "xml:space": "preserve",
+                width: "512px",
+                height: "512px",
+              },
+            },
+            [
+              _c("path", {
+                attrs: {
+                  d: "M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z",
+                },
+              }),
+            ]
+          ),
+        ]
+      ),
+    ]),
+  ])
 }
 var staticRenderFns = []
 render._withStripped = true
