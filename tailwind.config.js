03e432d8d3963d2bd30a81a07a08d35edf36de32
Gabriel Low <lowgabriel@ymail.com>
2021-12-07 22:58:14 +0800

    configured tailwind into project

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Roboto', 'Helvetica', 'Arial', 'sans-serif']
+    },
+    extend: {
+      fontFamily: ['hover', 'focus'],
+    },
   },
   variants: {
     extend: {},
