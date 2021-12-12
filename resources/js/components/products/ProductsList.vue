c254e10c84d472d5855c057c58b67899a89bfbf9
Gabriel Low <lowgabriel@ymail.com>
2021-12-12 14:09:35 +0800

    created layout for products list

--- resources/js/components/products/ProductsList.vue
+++ resources/js/components/products/ProductsList.vue
@@ -1,5 +1,46 @@
 <template>
-    <div>
-        list of products
+    <div class="grid grid-cols-4 gap-4 mt-4">
+        <div class="card">
+            <img class="h-70" src="https://images.unsplash.com/photo-1577982787983-e07c6730f2d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=859&q=80" alt="image of product">
+            <div class="p-3 relative">
+                <a href="#" class="no-underline hover:underline font-bold text-xl cursor-pointer">A Fancy Item</a>
+                <p class="text-base">RM30</p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="h-70" src="https://images.unsplash.com/photo-1577982787983-e07c6730f2d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=859&q=80" alt="image of product">
+            <div class="p-3 relative">
+                <a href="#" class="no-underline hover:underline font-bold text-xl cursor-pointer">A Fancy Item</a>
+                <p class="text-base">RM30</p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="h-70" src="https://images.unsplash.com/photo-1577982787983-e07c6730f2d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=859&q=80" alt="image of product">
+            <div class="p-3 relative">
+                <a href="#" class="no-underline hover:underline font-bold text-xl cursor-pointer">A Fancy Item</a>
+                <p class="text-base">RM30</p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="h-70" src="https://images.unsplash.com/photo-1577982787983-e07c6730f2d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=859&q=80" alt="image of product">
+            <div class="p-3 relative">
+                <a href="#" class="no-underline hover:underline font-bold text-xl cursor-pointer">A Fancy Item</a>
+                <p class="text-base">RM30</p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="h-70" src="https://images.unsplash.com/photo-1577982787983-e07c6730f2d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=859&q=80" alt="image of product">
+            <div class="p-3 relative">
+                <a href="#" class="no-underline hover:underline font-bold text-xl cursor-pointer">A Fancy Item</a>
+                <p class="text-base">RM30</p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="h-70" src="https://images.unsplash.com/photo-1577982787983-e07c6730f2d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=859&q=80" alt="image of product">
+            <div class="p-3 relative">
+                <a href="#" class="no-underline hover:underline font-bold text-xl cursor-pointer">A Fancy Item</a>
+                <p class="text-base">RM30</p>
+            </div>
+        </div>
     </div>
 </template>
