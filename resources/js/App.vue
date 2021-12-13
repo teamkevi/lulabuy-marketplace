34e9f20dd39d84957a7770beea1868a9ad68b49f
Gabriel Low <lowgabriel@ymail.com>
2021-12-13 17:15:46 +0800

    made search bar into a separate component

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="mx-20">
         <the-header></the-header>
+        <search-bar></search-bar>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue'
+import SearchBar from './components/Searchbar.vue'
 
 export default {
     components: {
-        TheHeader
+        TheHeader,
+        SearchBar
     }
 }
 </script>
