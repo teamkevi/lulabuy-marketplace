7432cdfd819b92b12b1c8f10c84444034f645204
Gabriel Low <lowgabriel@ymail.com>
2021-12-13 17:14:34 +0800

    added router link

--- resources/js/components/TheHeader.vue
+++ resources/js/components/TheHeader.vue
@@ -1,14 +1,20 @@
 <template>
-    <div class="flex justify-between text-lg py-4">
+    <nav class="flex justify-between text-lg py-4">
         <div>LOGO</div>
         <ul class="flex font-normal items-center text-gray-700">
-            <li class="mr-12 cursor-pointer hover:font-bold">Home</li>
-            <li class="mr-12 cursor-pointer hover:font-bold">Shops</li>
-            <li class="mr-12 cursor-pointer hover:font-bold">Products</li>
+            <li class="mr-12 cursor-pointer hover:font-bold">
+                <router-link :to="{name: 'index'}">Home</router-link>
+            </li>
+            <li class="mr-12 cursor-pointer hover:font-bold">
+                <router-link :to="{name: 'shops'}">Shops</router-link>
+            </li>
+            <li class="mr-12 cursor-pointer hover:font-bold">
+                <router-link :to="{name: 'products'}">Products</router-link>
+            </li>
             <li class="mr-12 cursor-pointer hover:font-bold">Cart</li>
             <li>
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign Up</button>
             </li>
         </ul>
-    </div>
+    </nav>
 </template>
