b5e99a1d17c6828e86e475886248e8168b5ddf28
Gabriel Low <lowgabriel@ymail.com>
2021-12-13 17:14:47 +0800

    added Shops page

---
+++ resources/js/pages/Shops.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="mt-4">
+        <shops-list></shops-list>
+    </div>
+</template>
+
+<script>
+import ShopsList from '../components/shops/ShopsList'
+
+export default {
+    components: {
+        ShopsList
+    }
+}
+</script>
