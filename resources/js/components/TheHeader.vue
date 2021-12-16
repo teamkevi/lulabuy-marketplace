c271ae36dd8c3b237bab0c53a0b69ddeb22f58fe
Gabriel Low <lowgabriel@ymail.com>
2021-12-16 16:00:16 +0800

    updated header to hide navigation items on registration page

--- resources/js/components/TheHeader.vue
+++ resources/js/components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="flex justify-between text-lg py-4">
         <div>LOGO</div>
-        <ul class="flex font-normal items-center text-gray-700">
+        <ul class="flex font-normal items-center text-gray-700" v-if="!hideNavigationItems">
             <li class="mr-12 cursor-pointer hover:font-bold">
                 <router-link :to="{name: 'index'}">Home</router-link>
             </li>
@@ -20,3 +20,13 @@
         </ul>
     </nav>
 </template>
+
+<script>
+export default {
+    computed: {
+        hideNavigationItems() {
+            return this.$route.name === 'register'
+        }
+    }
+}
+</script>
