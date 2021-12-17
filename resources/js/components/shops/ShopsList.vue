e020acfe1f7296ef33a10e3f9ec6527ee1a028e0
Gabriel Low <lowgabriel@ymail.com>
2021-12-17 13:01:34 +0800

    updated image width styling

--- resources/js/components/shops/ShopsList.vue
+++ resources/js/components/shops/ShopsList.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="grid grid-cols-4 gap-4">
         <div class="card">
-            <a href="#"><img class="h-70" src="https://images.unsplash.com/photo-1577982787983-e07c6730f2d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=859&q=80" alt="image of product"></a>
+            <a href="#"><img class="w-full" src="https://images.unsplash.com/photo-1577982787983-e07c6730f2d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=859&q=80" alt="image of product"></a>
             <div class="p-3">
                 <p><a href="#" class="no-underline hover:underline font-bold text-xl cursor-pointer">A Fancy Shop</a></p>
                 <p><a href="#" class="no-underline hover:underline text-base cursor-pointer">Bukit Jalil, Kuala Lumpur</a></p>
