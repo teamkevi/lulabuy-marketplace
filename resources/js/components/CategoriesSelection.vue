e6eaa79ec3b9475bf2b3b5d35932b62927c7b33d
Gabriel Low <lowgabriel@ymail.com>
2021-12-22 00:05:12 +0800

    temporarily removed categories selection

--- resources/js/components/CategoriesSelection.vue
+++ resources/js/components/CategoriesSelection.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="-mt-4 grid grid-flow-col gap-2">
+    <div></div>
+    <!-- <div class="-mt-4 grid grid-flow-col gap-2">
         <div class="card text-base p-3 cursor-pointer">Category 1</div>
         <div class="card text-base p-3 cursor-pointer">Category 2</div>
         <div class="card text-base p-3 cursor-pointer">Category 3</div>
         <div class="card text-base p-3 cursor-pointer">Category 4</div>
         <div class="card text-base p-3 cursor-pointer">Category 5</div>
         <div class="card text-base p-3 cursor-pointer">Category 6</div>
-    </div>
+    </div> -->
 </template>
