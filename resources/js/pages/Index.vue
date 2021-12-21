967a4d313961d7c0aab7a8d53bef393d959fd71d
Gabriel Low <lowgabriel@ymail.com>
2021-12-21 22:51:37 +0800

    imported searchbar

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <search-bar placeholder="Search for anything"></search-bar>
         <categories-selection></categories-selection>
         <h1 class="text-2xl mt-8 mb-3 font-bold">Featured Products</h1>
         <products-list></products-list>
@@ -12,12 +13,14 @@
 import CategoriesSelection from '../components/CategoriesSelection'
 import ProductsList from '../components/products/ProductsList'
 import ShopsList from '../components/shops/ShopsList'
+import SearchBar from '../components/SearchBar'
 
 export default {
     components: {
         CategoriesSelection,
         ProductsList,
-        ShopsList
+        ShopsList,
+        SearchBar
     }
 }
 </script>
