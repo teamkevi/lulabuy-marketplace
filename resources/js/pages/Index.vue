4d173ed97bd6b45b7298b7b95984172fef9fb10b
Gabriel Low <lowgabriel@ymail.com>
2021-12-22 18:27:42 +0800

    inverted shop list page

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -3,10 +3,11 @@
         <search-bar placeholder="Search for anything"></search-bar>
         <img src="img/marketplace-banner.jpg" alt="lulabuy banner">
         <categories-selection></categories-selection>
-        <h1 class="text-2xl mt-8 mb-3 font-bold">Featured Products</h1>
-        <products-list></products-list>
+        
         <h1 class="text-2xl mt-8 mb-3 font-bold">Featured Shops</h1>
         <shops-list></shops-list>
+        <h1 class="text-2xl mt-8 mb-3 font-bold">Featured Products</h1>
+        <products-list></products-list>
     </div>
 </template>
 
