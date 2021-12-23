044ce8632f92fc03ee79884159244403e32b167b
Gabriel Low <lowgabriel@ymail.com>
2021-12-23 18:45:01 +0800

    added route to product overview page

--- resources/js/components/ProductsListItem.vue
+++ resources/js/components/ProductsListItem.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="card">
-        <a href="#"><img class="w-full h-72" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/assortment-of-colorful-ripe-tropical-fruits-top-royalty-free-image-995518546-1564092355.jpg" alt="image of product"></a>
+        <router-link :to="`/shops/${shop_slug}/products/${product_slug}`"><img class="w-full h-72" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/assortment-of-colorful-ripe-tropical-fruits-top-royalty-free-image-995518546-1564092355.jpg" alt="image of product"></router-link>
         <div class="p-3">
-            <p><a href="#" class="no-underline hover:underline font-bold text-xl cursor-pointer">{{product_name}}</a></p>
+            <p><router-link :to="`/shops/${shop_slug}/products/${product_slug}`" class="no-underline hover:underline font-bold text-xl cursor-pointer">{{product_name}}</router-link></p>
             <p v-if="shop_name"><router-link :to="`/shops/${shop_slug}`" class="no-underline hover:underline text-base cursor-pointer">{{shop_name}}</router-link></p>
             <p class="text-base">RM {{price}}</p>
         </div>
@@ -13,6 +13,7 @@
 export default {
     props: {
         product_name: String,
+        product_slug: String,
         shop_name: String,
         shop_slug: String,
         price: String
