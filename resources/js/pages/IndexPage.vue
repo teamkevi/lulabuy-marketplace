0a8c3451fb75755cf1b90f71d81d62ab291d8eb6
Gabriel Low <lowgabriel@ymail.com>
2022-01-04 15:20:10 +0800

    added shop list in index page

--- resources/js/pages/IndexPage.vue
+++ resources/js/pages/IndexPage.vue
@@ -4,10 +4,24 @@
         <img src="img/marketplace-banner.jpg" alt="lulabuy banner">
         <categories-selection></categories-selection>
         <h1 class="text-2xl mt-8 mb-3 font-bold">Featured Shops</h1>
-        <shops-list></shops-list>
+        <div class="grid grid-cols-4 gap-4">
+            <shops-list-item
+                v-for="shop in shops" :key="shop.id" 
+                v-bind:name = "shop.name"
+                v-bind:slug = "shop.slug"
+                v-bind:address = "shop.address"
+            ></shops-list-item>
+        </div>
         <h1 class="text-2xl mt-8 mb-3 font-bold">Featured Products</h1>
         <div class="grid grid-cols-4 gap-4">
-            <products-list-item></products-list-item>   
+            <products-list-item
+                v-for="product in products" :key="product.id" 
+                v-bind:product_name = "product.name"
+                v-bind:product_slug = "product.slug"
+                v-bind:shop_name = "product.shop.name"
+                v-bind:shop_slug = "product.shop.slug"
+                v-bind:price = "product.price"
+            ></products-list-item>
         </div>
     </div>
 </template>
@@ -15,15 +29,29 @@
 <script>
 import CategoriesSelection from '../components/CategoriesSelection'
 import ProductsListItem from '../components/ProductsListItem'
-import ShopsList from '../components/shops/ShopsList'
+import ShopsListItem from '../components/ShopsListItem'
 import SearchBar from '../components/SearchBar'
 
 export default {
     components: {
         CategoriesSelection,
         ProductsListItem,
-        ShopsList,
+        ShopsListItem,
         SearchBar
+    },
+    data() {
+        return {
+            products: null,
+            shops: null
+        }
+    },
+    created() {
+        axios.get("/api/products").then(response => {
+            this.products = response.data;
+        });
+        axios.get("/api/shops").then(response => {
+            this.shops = response.data;
+        });
     }
 }
 </script>
