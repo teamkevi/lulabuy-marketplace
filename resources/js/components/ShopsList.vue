7b2c5d99f41da89a376ad0463e059d580a0350f7
Gabriel Low <lowgabriel@ymail.com>
2022-01-05 18:42:49 +0800

    added shops list component

---
+++ resources/js/components/ShopsList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="grid grid-cols-5 gap-4">
+        <div class="card" v-for="shop in shops" :key="shop.id">
+            <router-link :to="`/shops/${shop.slug}`">
+                <img class="w-full h-64" src="https://cdn5.vectorstock.com/i/1000x1000/73/14/letter-s-on-shop-logo-design-vector-35087314.jpg" alt="image of product">
+            </router-link>
+            <div class="p-3">
+                <router-link :to="`/shops/${shop.slug}`">
+                    <p class="no-underline hover:underline font-bold text-xl cursor-pointer">{{shop.name}}</p>
+                </router-link>
+                <p class="text-base">{{shop.address}}</p>
+                <p class="text-base">3.3km</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        shops: Array
+    }
+}
+</script>
