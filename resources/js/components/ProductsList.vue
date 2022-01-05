e8069ca2510acacce75813184a20795b33f9a4d7
Gabriel Low <lowgabriel@ymail.com>
2022-01-05 18:42:33 +0800

    updated image height

--- resources/js/components/ProductsList.vue
+++ resources/js/components/ProductsList.vue
@@ -4,7 +4,7 @@
             class="card"
             v-for="product in products" :key="product.id"
         >
-            <router-link :to="`/shops/${product.shop.slug}/products/${product.slug}`"><img class="w-full h-60" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/assortment-of-colorful-ripe-tropical-fruits-top-royalty-free-image-995518546-1564092355.jpg" alt="image of product"></router-link>
+            <router-link :to="`/shops/${product.shop.slug}/products/${product.slug}`"><img class="w-full h-64" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/assortment-of-colorful-ripe-tropical-fruits-top-royalty-free-image-995518546-1564092355.jpg" alt="image of product"></router-link>
             <div class="p-3">
                 <p><router-link :to="`/shops/${product.shop.slug}/products/${product.slug}`" class="no-underline hover:underline font-bold text-xl cursor-pointer">{{product.name}}</router-link></p>
                 <p v-if="product.shop.name"><router-link :to="`/shops/${product.shop.slug}`" class="no-underline hover:underline text-base cursor-pointer">{{product.shop.name}}</router-link></p>
